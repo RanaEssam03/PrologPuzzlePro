34e010c7d3b7663ae0218cf098654d41b29b602b
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-04-28 13:38:32 +0300

    added comments and fixed some problems

--- problem1.pl
+++ problem1.pl
@@ -1,9 +1,8 @@
 % Input
 input([
-    [red, red, blue],
-    [red, red, yellow],
-    [blue, yellow, yellow],
-    [blue, yellow, yellow]
+    [red, red, blue , blue],
+    [red, red, yellow , yellow],
+    [blue, yellow, yellow , yellow]
 ]).
 
 
@@ -14,7 +13,7 @@ move([X,Y], [X1,Y]) :- X1 is X - 1, valid(X1,Y).
 move([X,Y], [X,Y1]) :- Y1 is Y - 1, valid(X,Y1).
 
 % Check if cell is valid heigh:N width:M
-valid(X,Y) :- input(Board), length(Board, N), length(Board, M), X > 0, Y > 0, X =< N, Y =< M.
+valid(X,Y) :- input(Board), length(Board, N),nth1(1, Board, FirstRow),length(FirstRow , M), X > 0, Y > 0, X =< N, Y =< M.
 
 % Define colors
 color(red).
@@ -29,16 +28,16 @@ color_of_cell([X,Y], Color) :-
 
 % Define cycle of color
 color_cycle([X,Y], Color, Visited, Path) :-
-    color_of_cell([X,Y], Color),
-    \+ member([X,Y], Visited),
-    append([[X,Y]], Visited, NewVisited),
-    move([X,Y], [X1,Y1]),
-    color_cycle([X1,Y1], Color, NewVisited, NewPath),
-    append([[X,Y]], NewPath, Path).
+    color_of_cell([X,Y], Color),  % Check if cell is of the right color
+    \+ member([X,Y], Visited),   % Check if cell has not been visited
+    append([[X,Y]], Visited, NewVisited), % Add cell to visited list
+    move([X,Y], [X1,Y1]), % Move to next cell
+    color_cycle([X1,Y1], Color, NewVisited, NewPath), % Recursively check next cell
+    append([[X,Y]], NewPath, Path). % Add cell to path
 
 color_cycle([X,Y], Color, Visited, [[X,Y]]) :-
-    color_of_cell([X,Y], Color),
-    \+ member([X,Y], Visited).
+    color_of_cell([X,Y], Color),  % Check if cell is of the right color
+    \+ member([X,Y], Visited).  % Check if cell has not been visited
 
 % Check if cycle exists
 cycle_exists(Color, Path) :-
@@ -47,17 +46,17 @@ cycle_exists(Color, Path) :-
     Len >= 4.
 
 % Search for color cycles
-search_color_cycles(Color) :-
-    cycle_exists(Color, Path),
+search_color_cycles(Color) :- % If cycle exists, print it and stop
+    cycle_exists(Color, Path), 
     write('Found a '), write(Color), write(' cycle: '), write(Path), nl, !.
 
-search_color_cycles(_).
+search_color_cycles(_). % Do nothing if no cycle exists
 
 % Main predicate to find all color cycles
 find_color_cycles :-
     color(Color),
     search_color_cycles(Color),
-    fail.
+    fail. % Fail to backtrack and find all cycles
 
 find_color_cycles :-
     write('No cycles exist.'), nl.
