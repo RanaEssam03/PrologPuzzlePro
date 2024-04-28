001778df9020e568170f269ddded5a51cebaa643
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-04-28 12:18:29 +0300

    started problem1

--- problem1.pl
+++ problem1.pl
@@ -1,41 +1,63 @@
-search(Open, Closed, Goal):-
-    getState(Open, [CurrentState,Parent], _), % Step 1
-    CurrentState = Goal, !, % Step 2
-    write("Search is complete!"), nl,
-    printSolution([CurrentState,Parent], Closed).
+% Input
+input([
+    [red, red, blue],
+    [red, red, yellow],
+    [blue, yellow, yellow],
+    [blue, yellow, yellow]
+]).
 
 
-search(Open, Closed, Goal):-
-    getState(Open, CurrentNode, TmpOpen),
-    getAllValidChildren(CurrentNode,TmpOpen,Closed,Children), % Step3
-    addChildren(Children, TmpOpen, NewOpen), % Step 4
-    append(Closed, [CurrentNode], NewClosed), % Step 5.1
-    search(NewOpen, NewClosed, Goal). % Step 5.2
+% Moves
+move([X,Y], [X1,Y]) :- X1 is X + 1, valid(X1,Y).
+move([X,Y], [X,Y1]) :- Y1 is Y + 1, valid(X,Y1).
+move([X,Y], [X1,Y]) :- X1 is X - 1, valid(X1,Y).
+move([X,Y], [X,Y1]) :- Y1 is Y - 1, valid(X,Y1).
 
-% Implementation of step 3 to get the next states
-getAllValidChildren(Node, Open, Closed, Children):-
-    findall(Next, getNextState(Node, Open, Closed, Next), Children).
+% Check if cell is valid heigh:N width:M
+valid(X,Y) :- input(Board), length(Board, N), length(Board, M), X > 0, Y > 0, X =< N, Y =< M.
 
-getNextState([State,_], Open, Closed, [Next,State]):-
-    move(State, Next),
-    not(member([Next,_], Open)),
-    not(member([Next,_], Closed)).
-% Implementation of getState and addChildren determine the search alg.
-% BFS
+% Define colors
+color(red).
+color(yellow).
+color(blue).
 
-getState([CurrentNode|Rest], CurrentNode, Rest).
-addChildren(Children, Open, NewOpen):-
-    append(Open, Children, NewOpen).
+% Define color of a cell
+color_of_cell([X,Y], Color) :- 
+    input(Board), 
+    nth1(X, Board, Row), 
+    nth1(Y, Row, Color).
 
-% Implementation of printSolution to print the actual solution path
-printSolution([State, null],_):-
-    write(State), nl.
+% Define cycle of color
+color_cycle([X,Y], Color, Visited, Path) :-
+    color_of_cell([X,Y], Color),
+    \+ member([X,Y], Visited),
+    append([[X,Y]], Visited, NewVisited),
+    move([X,Y], [X1,Y1]),
+    color_cycle([X1,Y1], Color, NewVisited, NewPath),
+    append([[X,Y]], NewPath, Path).
 
+color_cycle([X,Y], Color, Visited, [[X,Y]]) :-
+    color_of_cell([X,Y], Color),
+    \+ member([X,Y], Visited).
 
-printSolution([State, Parent], Closed):-
-    member([Parent, GrandParent], Closed),
-    printSolution([Parent, GrandParent], Closed),
-    write(State), nl.
+% Check if cycle exists
+cycle_exists(Color, Path) :-
+    color_cycle([1,1], Color, [], Path),
+    length(Path, Len),
+    Len >= 4.
 
-move(State, Next):-
-%% here we implement the logic of the problem 
+% Search for color cycles
+search_color_cycles(Color) :-
+    cycle_exists(Color, Path),
+    write('Found a '), write(Color), write(' cycle: '), write(Path), nl, !.
+
+search_color_cycles(_).
+
+% Main predicate to find all color cycles
+find_color_cycles :-
+    color(Color),
+    search_color_cycles(Color),
+    fail.
+
+find_color_cycles :-
+    write('No cycles exist.'), nl.
