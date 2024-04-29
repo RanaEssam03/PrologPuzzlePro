9ab5ecc240a40a343e152805724746456120079c
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-04-29 22:04:39 +0300

    finished

--- problem1.pl
+++ problem1.pl
@@ -2,8 +2,8 @@
 input([
     [red, red, blue],
     [red, red, yellow],
-    [blue, blue, yellow],
-    [blue, blue, yellow]
+    [yellow, yellow, yellow],
+    [yellow, yellow, yellow]
 ]).
 
 
@@ -14,8 +14,10 @@ move([X,Y], [X1,Y]) :- X1 is X - 1, valid(X1,Y).
 move([X,Y], [X,Y1]) :- Y1 is Y - 1, valid(X,Y1).
 
 % Check if cell is valid heigh:N width:M
-valid(X,Y) :- input(Board), length(Board, N), length(Board, M), X > 0, Y > 0, X =< N, Y =< M.
+valid(X,Y) :- input(Board), length(Board, N),nth1(1, Board, FirstRow), length(FirstRow, M), X > 0, Y > 0, X =< N, Y =< M.
 
+last(X, [X]) :- !.
+last(X, [_|T]) :- last(X, T).
 % Define colors
 color(red).
 color(yellow).
@@ -44,7 +46,7 @@ color_cycle([X,Y], Color, Visited, [[X,Y]]) :-
 
 cycle_exists(Color, Path):-
     color_cycle([X,Y] , Color,[],Path),
-    length(Path, N), N >= 4, append(_, [[X,Y]], Path).
+    length(Path, N), N >= 4,last([X1,Y1], Path),move([X,Y] , [X1,Y1]).
 
 % Search for color cycles
 search_color_cycles(Color) :-
